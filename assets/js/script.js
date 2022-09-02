49f62e42cef9f6f91f0304b81c0043cef519ca17
Caitriona71 <caitrionaboyle@ymail.com>
2022-09-02 17:05:02 +0000

    Add variables for quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function check (){
+    var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var q4 = document.quiz.q4.value;
+    var q5 = document.quiz.q5.value;
+    var q6 = document.quiz.q6.value;
+    var q7 = document.quiz.q7.value;
+    var q8 = document.quiz.q8.value;
+    var q9 = document.quiz.q9.value;
+}
